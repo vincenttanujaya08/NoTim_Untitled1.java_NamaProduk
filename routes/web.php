b5b1488c2d65c7f56ec7aa32dbb2b2721161aac7
lindsay <gabriellelindsaysupradja@gmail.com>
2025-04-26 15:41:43 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('base.header');
+    return view('home');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
 });
 
 Route::get('/katalog', function () {
