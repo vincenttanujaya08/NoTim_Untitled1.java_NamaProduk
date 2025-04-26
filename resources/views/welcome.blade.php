62af82350b97a739ff386e5a19223c682a6920f2
lindsay <gabriellelindsaysupradja@gmail.com>
2025-04-26 09:50:35 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <h1>Welcome</h1>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
